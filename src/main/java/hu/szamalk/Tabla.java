bf94710a853b4e91d9c28ed40952097bc5d052a3
Bito-Zalan <bitozalan23@gmail.com>
2024-12-09 09:31:27 +0100

    f1 1

---
+++ src/main/java/hu/szamalk/Tabla.java
@@ -0,0 +1,31 @@
+package hu.szamalk;
+
+public class Tabla {
+    private char [][] T;
+    private char uresCella;
+
+    /*public Tabla(char uresCella) {
+        T = new char[8][8];
+        this.uresCella = uresCella;
+
+
+        for (int i = 0; i < T.length; i++) {
+            for (int j = 0; j < T.length; j++) {
+                T[i][j] = uresCella;
+            }
+        }
+    }*/
+
+    /*public void megjelenit(){
+        for (int i = 0; i < T.length; i++) {
+            for (int j = 0; j < T.length; j++) {
+                System.out.print(T[i][j]);
+            }
+            System.out.println("");
+        }
+    }*/
+
+
+}
+
+
